7314aeb54af4a80fd40ec3f81ad1f05ad6534538
santoshbishnoi <santoshsbishnoi6@gmail.com>
2021-04-25 01:05:51 +0530

    multiple charts

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-
-df=pd.read_csv('RELIANCE.NS.csv')
-# Remove the entries with 0 as open price
-df=df[df['Open'] != 0]
-df['PriceMvt']=df.Close-df.Open
-if df['PriceMvt']>15:
-    # print(df.PriceMvt)
-    print("hh")
